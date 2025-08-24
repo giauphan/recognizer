695f66762b5c5936181ca542a9d87a2d33d83642
giauphan <giauphan012@gmail.com>
2025-08-24 18:59:16 +0700

    wip

--- recognizer/components/detector.py
+++ recognizer/components/detector.py
@@ -59,13 +59,13 @@ class DetectionModels:
         self.vit_model = CLIPModel.from_pretrained("flavour/CLIP-ViT-B-16-DataComp.XL-s13B-b90K", device_map="cpu")
 
     def _load_vit_processor(self):
-        self.vit_processor = AutoProcessor.from_pretrained("flavour/CLIP-ViT-B-16-DataComp.XL-s13B-b90K")
+        self.vit_processor = CLIPProcessor.from_pretrained("flavour/CLIP-ViT-B-16-DataComp.XL-s13B-b90K")
 
     def _load_seg_model(self):
         self.seg_model = CLIPSegForImageSegmentation.from_pretrained("CIDAS/clipseg-rd64-refined", device_map="cpu")
 
     def _load_seg_processor(self):
-        self.seg_processor = AutoProcessor.from_pretrained("CIDAS/clipseg-rd64-refined")
+        self.seg_processor = CLIPSegProcessor.from_pretrained("CIDAS/clipseg-rd64-refined")
 
     
 
